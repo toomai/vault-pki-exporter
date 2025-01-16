b84d3136668a738364dc1483fdafcf0f52d19e41
Loïc Stevens <loic.stevens@worldline.com>
2025-01-16 19:21:10 +0100

    feat: add flag to scrape only CA

--- pkg/vault-mon/prometheus.go
+++ pkg/vault-mon/prometheus.go
@@ -130,9 +130,11 @@ func PromWatchCerts(pkimon *PKIMon, interval time.Duration) {
 				}
 				duration := time.Since(startTime).Seconds()
 				promWatchCertsDuration.Observe(duration)
-				slog.Info("PKI Prometheus metrics updated, sleeping", "pki", pkiname, "total_certs", len(pki.certs), "expired_certs", pki.expiredCertsCounter, "duration_seconds", duration, "interval", interval)
-				time.Sleep(interval)
+				slog.Info("PKI Prometheus metrics updated", "pki", pkiname, "total_certs", len(pki.certs), "expired_certs", pki.expiredCertsCounter, "duration_seconds", duration, "interval", interval)
 			}
+			duration := time.Since(startTime).Seconds()
+			slog.Info("PKI Prometheus metrics have all been updated, sleeping", "duration_seconds", duration)
+			time.Sleep(interval)
 		}
 	}()
 }
